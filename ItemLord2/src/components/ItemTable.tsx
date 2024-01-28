d5df680cf849a30965c09e4fd0d3b7d94f460e1e
Darius Hadjati <darius.hadjati@gmail.com>
2024-01-28 19:35:30 +0100

    feat: creating ItemTable components for market and pocket components

---
+++ ItemLord2/src/components/ItemTable.tsx
@@ -0,0 +1,107 @@
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { useDispatch, useSelector } from "react-redux";
+import { selectItem, setTradeType, toggleOverlay } from "../redux/actions";
+import { Item } from "../types";
+import { AppState } from "../redux/reducers";
+
+interface ItemTableProps {
+  items: Item[]; // Assuming Item is the type for items
+  tradeType: string;
+  title: string;
+  size: "small" | "medium";
+  ariaLabel: string;
+}
+
+const ItemTable: React.FC<ItemTableProps> = ({
+  items,
+  tradeType,
+  title,
+  size,
+  ariaLabel,
+}) => {
+  const dispatch = useDispatch();
+  const market = useSelector((state: AppState) => state.market);
+
+  const color = { color: "white", backgroundColor: "black" };
+  const getYesterdaysItemPrice = (itemName: string): number | null => {
+    const yesterDayItem = market.yesterdaysForSale.find(
+      (item) => item.name === itemName
+    );
+    return yesterDayItem ? yesterDayItem.price : null;
+  };
+  return (
+    <>
+      <h3 style={{ textAlign: "center" }}>{title}</h3>
+      <TableContainer
+        component={Paper}
+        sx={{
+          marginBottom: "5rem",
+          ...color,
+          maxHeight: 350,
+        }}
+      >
+        <Table stickyHeader size={size} aria-label={ariaLabel}>
+          <TableHead sx={{ position: "stickey" }}>
+            <TableRow>
+              <TableCell sx={{ backgroundColor: "white" }}>Name</TableCell>
+              <TableCell align="right" sx={{ backgroundColor: "white" }}>
+                Qty
+              </TableCell>
+              <TableCell align="right" sx={{ backgroundColor: "white" }}>
+                Price
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((item: Item) => (
+              <TableRow
+                onClick={() => {
+                  dispatch(setTradeType(tradeType));
+                  dispatch(selectItem(item));
+                  dispatch(toggleOverlay());
+                }}
+                key={item.name}
+                sx={{
+                  "&:last-child td, &:last-child th": { border: 0, ...color },
+                  cursor: "pointer",
+                }}
+              >
+                <TableCell component="th" scope="row" sx={{ ...color }}>
+                  {tradeType === "sell" &&
+                  getYesterdaysItemPrice(item.name) !== null ? (
+                    item.price > getYesterdaysItemPrice(item.name)! ? (
+                      <ArrowUpwardIcon sx={{ color: "green", fontSize: 20 }} />
+                    ) : item.price < getYesterdaysItemPrice(item.name)! ? (
+                      <ArrowDownwardIcon sx={{ color: "red", fontSize: 20 }} />
+                    ) : (
+                      "    "
+                    )
+                  ) : (
+                    "    "
+                  )}
+                  {item.name}
+                </TableCell>
+                <TableCell align="right" sx={{ ...color }}>
+                  {item.qty}
+                </TableCell>
+                <TableCell align="right" sx={{ ...color }}>
+                  {item.price}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+
+export default ItemTable;
--- ItemLord2/src/components/Pocket.tsx
+++ ItemLord2/src/components/Pocket.tsx
@@ -1,75 +1,23 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { getAllItemsQty } from "../utils";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { AppState } from "../redux/reducers";
-import { selectItem, setTradeType, toggleOverlay } from "../redux/actions";
+import ItemTable from "./ItemTable";
 
 export default function Pocket() {
-  const dispatch = useDispatch();
   const pocket = useSelector((state: AppState) => state.pocket);
   const pocketSize = useSelector((state: AppState) => state.pocketSize);
-
-  const color = { color: "white", backgroundColor: "black" };
-
+  const tableTitle = `Your Pants Pocket ${getAllItemsQty(
+    pocket
+  )}/${pocketSize}`;
   return (
     <div>
-      <h3 style={{ textAlign: "center" }}>
-        Your Pants Pocket ({getAllItemsQty(pocket)}/{pocketSize})
-      </h3>
-      <TableContainer
-        component={Paper}
-        sx={{
-          marginBottom: "5rem",
-          ...color,
-          maxHeight: 230,
-        }}
-      >
-        <Table stickyHeader size="small" aria-label="a dense table">
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ backgroundColor: "white" }}>Name</TableCell>
-              <TableCell align="right" sx={{ backgroundColor: "white" }}>
-                Qty
-              </TableCell>
-              <TableCell align="right" sx={{ backgroundColor: "white" }}>
-                Price
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {pocket.map((item) => (
-              <TableRow
-                onClick={() => {
-                  dispatch(setTradeType("sell"));
-                  dispatch(selectItem(item));
-                  dispatch(toggleOverlay());
-                }}
-                key={item.name}
-                sx={{
-                  "&:last-child td, &:last-child th": { border: 0, ...color },
-                  cursor: "pointer",
-                }}
-              >
-                <TableCell component="th" scope="row" sx={{ ...color }}>
-                  {item.name}
-                </TableCell>
-                <TableCell align="right" sx={{ ...color }}>
-                  {item.qty}
-                </TableCell>
-                <TableCell align="right" sx={{ ...color }}>
-                  {item.price}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <ItemTable
+        items={pocket}
+        tradeType={"buy"}
+        title={tableTitle}
+        size="small"
+        ariaLabel="a dense table"
+      />
     </div>
   );
 }
